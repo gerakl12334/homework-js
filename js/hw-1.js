12d2584e375f0a01cd42153370b6d0e54ae7f199
gerakl12334 <shtempel_vlad@zosh57.mk.ua>
2024-12-14 13:17:11 +0200

    hw-1.js

---
+++ js/hw-1.js
@@ -0,0 +1,31 @@
+// завданя-1
+const age = 14;
+console.log(age);
+// завданя-2
+const name = "Влад";
+console.log("мене звати: ", name);
+// завданя-3
+const isStudent = true;
+console.log(isStudent);
+// завданя-4
+const myString = "після бою кулаками не машуть";
+console.log(myString);
+// завданя-5
+let myNumber = 10;
+myNumber = myNumber + 10;
+console.log(myNumber);
+// завданя-6
+const myNull = null;
+console.log(myNull);
+// завданя-7
+let consoleName = prompt("введіть своє ім'я");
+console.log(consoleName);
+alert("привіт " + consoleName);
+// завданя-8
+let test = confirm("підтвердіть дію");
+if (test) {
+  alert("дякую за підтвердженя");
+} else {
+  alert("Дію відмінено!");
+}
+// завданя-9
